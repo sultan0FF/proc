a360489bbfbef42a8937efc1f6b09d39ef360aef
sultan0FF <dlaykontakta@rambler.ru>
2018-03-06 16:01:31 +0700

    2-5

---
+++ 1 laba - m/box_atd.h
@@ -0,0 +1,9 @@
+#ifndef __box_atd__
+#define __box_atd__
+namespace simple_shapes {
+  // параллелепипед
+  struct box {
+    int x, y, z; // ширина, высота, длина
+  };
+} // end simple_shapes namespace
+#endif
---
+++ 1 laba - m/shapes.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include "shape_atd.h"
+
+using namespace std;
+
+namespace simple_shapes {
+  // Сигнатуры требуемых внешних функций 
+  void In(box &r, ifstream &ist);
+  void In(share  &t, ifstream &ist);
+
+  // Ввод параметров обобщенной фигуры из файла
+  shape* In(ifstream &ifst)
+  {
+    shape *sp;
+    int k;
+    ifst >> k;
+    switch(k) {
+    case 1:
+      sp = new shape;
+      sp->k = shape::key::BOX;
+      In(sp->r, ifst);
+      return sp;
+    case 2:
+      sp = new shape;
+      sp->k = shape::key::SHERE;
+      In(sp->t, ifst);
+      return sp;
+    default:
+      return 0;
+    }
+  }
+
+  void Out(box &r, ofstream &ofst);
+  void Out(share  &t, ofstream &ofst);
+
+  // Вывод параметров текущей фигуры в поток
+  void Out(shape &s, ofstream &ofst) {
+    switch(s.k) {
+    case shape::key::BOX:
+      Out(s.r, ofst);
+      break;
+    case shape::key::SHERE:
+      Out(s.t, ofst);
+      break;
+    default:
+      ofst << "Incorrect figure!" << endl;
+    }
+  }
+} // end simple_shapes namespace
+
+
